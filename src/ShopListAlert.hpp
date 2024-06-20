3fee7fe7f2cbe3e7ae4c65af27d65600a823411e
ML5 <Fireluis500@gmail.com>
2024-06-20 16:56:55 -0400

    Testing something.

--- src/ShopListAlert.hpp
+++ src/ShopListAlert.hpp
@@ -9,12 +9,10 @@ class ShoppingListAlert : public Popup<> {
         size_t m_currentPage = 1;
         std::array<int, 5> m_itemTotal = {41, 38, 72, 40, 80};
         std::array<int, 5> m_itemCount = {0, 0, 0, 0, 0};
-
-
         std::array<int, 5> m_tagged = {0, 0, 0, 0, 0};
+        
         //  std::set<int> m_taggedItems = {};
-
-        std::vector<int> m_taggedItems = {};
+       std::vector<int> m_taggedItems = {};
 
         bool m_selectMode = false;
         int m_totalManaOrbs = 0;
